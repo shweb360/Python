3fb31f58740ca8c85b8e846068325d7abc17be01
shweb360 <785132826@qq.com>
2017-09-25 17:02:05 +0800

    读写文件

---
+++ 25_IO.py
@@ -0,0 +1,13 @@
+
+#1、读取文件
+f=open('F:/03学习区/13Python/02Demo/教程地址.txt','r')
+content=f.read();
+print(content);
+f.close()
+
+#2、写入文件
+
+f=open('F:/03学习区/13Python/02Demo/教程地址.txt','w')
+content=f.write('wushuang');
+print(content);
+f.close()
