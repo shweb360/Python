5c087cb324263672d449ba288684095fcbd9b80e
shweb360 <785132826@qq.com>
2018-01-26 14:45:07 +0800

    复习

--- 25_IO.py
+++ 25_IO.py
@@ -6,7 +6,6 @@ print(content);
 f.close()
 
 #2、写入文件
-
 f=open('F:/03学习区/13Python/02Demo/教程地址.txt','w')
 content=f.write('wushuang');
 print(content);
---
+++ 27_StringList.py
@@ -0,0 +1,12 @@
+word='wushuang'
+
+print(word[:1])
+
+list=[12,3,45,65,78]
+print(list[-1])
+
+a,b=1,2
+while b<10:
+	print(b)
+	a,b=b,a+b
+ 
