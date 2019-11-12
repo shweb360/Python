f3730dc68192c9759ad30833d4568f84c3394c5a
wush <1069680815@qq.com>
2019-11-12 16:07:03 +0800

    Signed-off-by: wush <1069680815@qq.com>

--- mystie/mysite/cltuserinfo/urls.py
+++ mystie/mysite/cltuserinfo/urls.py
@@ -1,13 +1,19 @@
 from django.conf.urls import url
 from django.urls import path,include
-from . import views,testdb
+from . import views,testdb,search
 
 urlpatterns = [
    url(r'^$', views.index),
    path('about/',views.about),
-   path('hello/', views.hello),   
+   path('hello/', views.hello),  
+
+   #数据库操作 
    path('add/', testdb.add),
    path('query/', testdb.query),
    path('update/', testdb.update),
    path('delete/', testdb.delete),
+
+   #表单
+   path('search_form/',search.search_form),
+   path('search/', search.search),
 ]
