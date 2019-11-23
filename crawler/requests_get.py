1fe82c761b435ec3ea5f2bcd8087932420375948
wush <1069680815@qq.com>
2019-11-23 23:53:28 +0800

    get的基本用法

--- crawler/requests_get.py
+++ crawler/requests_get.py
@@ -2,10 +2,10 @@
 import requests
 
 resp=requests.get('http://www.baidu.com/')
-# print(resp.text)
-# print(resp.content)
-# print(resp.cookies)
-# print(resp.status_code)
-# print(resp.url)
+print(resp.text)
+print(resp.content)
+print(resp.cookies)
+print(resp.status_code)
+print(resp.url)
 print(resp.headers)
 print(resp.history)
