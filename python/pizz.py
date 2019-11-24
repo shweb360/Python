ab644677e3bf6fac26843a0ab1ac7fbafa7a8b5e
wush <1069680815@qq.com>
2019-11-24 10:01:39 +0800

    导入模块

---
+++ python/pizz.py
@@ -0,0 +1,7 @@
+def make_pizza(size,*toppings):
+    #概述要制作的比萨
+    print('\nMaking a '+str(size)+
+        '-inch pizza with the flowing toppings:'
+    )
+    for topping in toppings:
+        print("-"+topping)
---
+++ python/pizz_making.py
@@ -0,0 +1,4 @@
+import pizz
+#导入模块，调用函数
+pizz.make_pizza(16,'peperoin')
+pizz.make_pizza(12,'mushroom','green peppers','extra cheese')
